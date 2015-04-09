7395c2d501cd4f02022bf54c1f925a5c099b0233
szensk <szensk@kozilek.me>
2015-04-09 11:54:20 -0500

    You are now floating. (collision callbacks)

--- gravity/main.lua
+++ gravity/main.lua
@@ -1,3 +1,6 @@
+-- physics playground
+-- i'll work on this until I get platformer physics to work in Box2D.
+
 -- libraries
 local sti    = require 'sti'
 local push   = require 'inpush'
@@ -13,7 +16,25 @@ local player = {
   sy = 28 * 16,
   w = 16,
   h = 32,
+  floating = true
 }
+
+local function beginContact(a, b, collision)
+  if b:getUserData() == player then
+    player.floating = false
+    local nx, ny = collision:getNormal()
+    print(nx, ny)
+    -- when you jump and hit a block
+    -- unfortunately need to handle when the collision 
+    if nx == -1 and ny == 0 then player.floating = true end
+  end
+end
+
+local function endContact(a, b, collision)
+  if b:getUserData() == player then
+    player.floating = true
+  end
+end
 
 local function create_world(args)
   local meter = args.meter or 16
@@ -21,15 +42,29 @@ local function create_world(args)
   love.physics.setMeter(meter)
   local world = love.physics.newWorld(0,0)
   world:setGravity(0, gravity)
+  world:setCallbacks(beginContact, endContact)
   return world
 end
 
 local function create_terrain(map)
   local terrain = map:initWorldCollision(world)
     for i,c in ipairs(terrain) do
-    c.fixture:setFriction(1)
+    --c.fixture:setFriction(5)
   end
   return terrain
+end
+
+local function set_controls()
+  push.bind('a', function() player.body:applyForce(-400, 0) end)
+  push.bind('d', function() player.body:applyForce(400, 0) end)
+  push.bind('w', function() -- jump
+    if not player.floating then 
+      player.body:applyForce(0, -6000) 
+    end 
+  end)
+  push.bind('s', function() end) --drop down
+  push.bind('q', action.toggleCollision)
+  push.bind('escape', function() love.event.quit() end)
 end
 
 function love.load()
@@ -41,27 +76,28 @@ function love.load()
   world = create_world{meter = 16}
   collision = create_terrain(map)
   player.body = love.physics.newBody(world, player.sx, player.sy, "dynamic")
+  --player.body:setUserData(player)
   player.body:setMass(1000)
   player.body:setFixedRotation(true)
   player.shape   = love.physics.newRectangleShape(player.w - 2, player.h -2 )
   player.fixture = love.physics.newFixture(player.body, player.shape, 1)
-  -- input
-  push.bind('a', function() player.body:applyForce(-400, 0) end)
-  push.bind('d', function() player.body:applyForce(400, 0)  end)
-  push.bind('w', function() player.body:applyForce(0, -1337) end)
-  push.bind('s', function() end)
-  push.bind('q', action.toggleCollision)
-  push.bind('escape', function() love.event.quit() end)
+  player.fixture:setUserData(player)
+  --input
+  set_controls()
 end
 
 function love.draw()
   local tx = love.graphics.getWidth()/2 - (player.body:getX() + player.w/2)
   local ty = love.graphics.getHeight()/2 - (player.body:getY() + player.h/2)
+  tx, ty = math.floor(tx), math.floor(ty) --removes jitter from subpixel alignment
   love.graphics.translate(tx, ty)
   map:draw()
   local r,g,b,a = love.graphics.getColor()
-  -- debug player box, in blue
+  -- debug player info, in blue
   love.graphics.setColor(0, 162, 232)
+  love.graphics.print( ("x: %0.0f y: %0.0f"):format(player.body:getPosition()), -tx, -ty)
+  --collision box
+  love.graphics.print("floating: " .. tostring(player.floating), player.body:getX()-32, player.body:getY()-32)
   love.graphics.polygon("line", player.body:getWorldPoints(player.shape:getPoints()))
   -- debug collision map, in red
   if showCollide then 
@@ -75,6 +111,7 @@ function love.update(dt)
   map:update(dt)
   world:update(dt)
   push.update(dt)
+  local lvx, lvy = player.body:getLinearVelocity()
   love.window.setTitle("gravity: " .. tostring(love.timer.getFPS()))
   if action.toggleCollision.pressed then
     showCollide = not showCollide
